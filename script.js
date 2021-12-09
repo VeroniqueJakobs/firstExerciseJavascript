9e85f0f2f553579ad3c36cf7604618b9f8b8fc63
VeroniqueJakobs <93218554+VeroniqueJakobs@users.noreply.github.com>
2021-12-09 15:12:45 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+ // Comment: This is my first JavaScript code 
+ console.log("Hello Winc Academy");
+
+ let firstName = "Veronique";
+ console.log(firstName);
+
+ //Variable called 'if' doesn't work, cause it is a keyword in Javascript.
+
+ let numbers = '4 + 4';
+ console.log(numbers);
+
+ let add = "4" + "4";
+ console.log(add);
+
+ // Result in console 'numbers'is 4 + 4    Result 'add' in console is 44
+
+ // Some simple sums
+ let Addition = 4 + 4;
+ console.log(Addition);
+
+ let  Subtraction = 4 - 4;
+ console.log(Subtraction);
+
+ let Multiplication = 4 * 4;
+ console.log(Multiplication);
+
+ let Division = 4 / 4;
+ console.log(Division);
+
+ let Modulus = 4 % 100;
+ console.log(Modulus);
+
+ console.log(typeof 35);
+ console.log('typeof 35');
